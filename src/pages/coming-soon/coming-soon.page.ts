fa119b747d86f9c52ba9af04b769b36a3e4aeb2e
Coen Mooij <coenmooij@gmail.com>
2025-09-19 10:32:49 +0200

    add coming soon

---
+++ src/pages/coming-soon/coming-soon.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ImageComponent } from '@coenmooij/ui';
+
+@Component({
+  selector: 'cm-coming-soon',
+  templateUrl: 'coming-soon.page.html',
+  imports: [
+    ImageComponent,
+  ],
+  styleUrl: 'coming-soon.page.scss',
+})
+export class ComingSoonPage {}
