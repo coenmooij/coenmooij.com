57daf1620d964e2a4055cb37907d7cf9f603202e
Coen Mooij <coenmooij@gmail.com>
2025-09-19 10:34:16 +0200

    remove coming soon

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { ComingSoonPage, HomePage } from '@coenmooij/pages';
+import { HomePage } from '@coenmooij/pages';
 import { PageLayout } from './layout';
 
 export const routes: Routes = [
-  {path: '**', component: ComingSoonPage},
   {
     path: '', component: PageLayout, children: [
       {path: '', pathMatch: 'full', component: HomePage},
